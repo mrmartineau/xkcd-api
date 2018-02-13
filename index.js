854691704fce16eeebe319b29b004db214982eab
Zander Martineau <zander@martineau.tv>
2018-02-13 20:15:34 +0000

    improve CORS and add retina images

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const axios = require('axios')
-const { json, send } = require('micro')
+const { send } = require('micro')
+const microCors = require('micro-cors')
+const cors = microCors({ allowMethods: ['GET'] })
 const DOMAIN = 'https://xkcd.com/'
 const PATH = 'info.0.json'
 
-module.exports = async function(req, res) {
-  const id = req.url.replace('/', '')
+const handler = async function(req, res) {
+  let id = req.url.replace('/', '')
   const comicId = id ? `${id}/` : ''
   const path = `${DOMAIN}${comicId}${PATH}`
   const response = await axios(path)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  send(res, 200, response.data)
+  id = response.data.num
+
+  let newResponse
+  if (id >= 1084) {
+    newResponse = {
+      ...response.data,
+      imgRetina: `${response.data.img.replace('.png', '')}_2x.png`,
+    }
+  } else {
+    newResponse = {
+      ...response.data,
+    }
+  }
+  send(res, 200, newResponse)
 }
+
+module.exports = cors(handler)
